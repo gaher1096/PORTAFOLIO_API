2a38498dbb32d810d47ee381808a40056935d715
Gabriel Ordoñez <gaher1096@hotmail.com>
2024-10-08 23:13:46 -0500

    Se agrego en la ruta index el prefijo api

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -5,9 +5,9 @@ import { authRoutes } from "./authRoutes";
 const router = Router();
 
 // Usar las rutas públicas
-router.use("/", publicRoutes);
+router.use("/api/", publicRoutes);
 
 // Usar las rutas de autenticación
-router.use("/auth", authRoutes);
+router.use("/api/auth", authRoutes);
 
 export { router as indexRoutes };
